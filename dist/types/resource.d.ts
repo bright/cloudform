26c54528ce4672936882fb7038e92796da00f250
adambar <adam.bar@brightinventions.pl>
2017-11-23 09:29:15 +0100

    Define Resource policies & Metadata properties

--- dist/types/resource.d.ts
+++ dist/types/resource.d.ts
@@ -1,10 +1,42 @@
 import { Value } from "./dataTypes";
+export interface CreationPolicy {
+    AutoScalingCreationPolicy?: {
+        MinSuccessfulInstancesPercent?: Value<number>;
+    };
+    ResourceSignal?: {
+        Count?: Value<number>;
+        Timeout?: Value<string>;
+    };
+}
+export declare type DeletionPolicy = 'Delete' | 'Retain' | 'Snapshot';
+export interface UpdatePolicy {
+    AutoScalingReplacingUpdate?: {
+        WillReplace?: Value<boolean>;
+    };
+    AutoScalingRollingUpdate?: {
+        MaxBatchSize?: Value<number>;
+        MinInstancesInService?: Value<number>;
+        MinSuccessfulInstancesPercent?: Value<number>;
+        PauseTime?: Value<string>;
+        SuspendProcesses?: Value<string>[];
+        WaitOnResourceSignals?: Value<boolean>;
+    };
+    AutoScalingScheduledAction?: {
+        IgnoreUnmodifiedGroupSizeProperties?: Value<boolean>;
+    };
+}
 export default interface Resource {
     Type: string;
     Properties?: {
         [key: string]: any;
     };
     DependsOn?: Value<string> | Value<string>[];
+    Metadata?: {
+        [key: string]: any;
+    };
+    CreationPolicy?: CreationPolicy;
+    DeletionPolicy?: DeletionPolicy;
+    UpdatePolicy?: UpdatePolicy;
 }
 export declare class ResourceBase {
     Type: string;

f5bd935262cff4c0f6397c2f1f77db502541885d
adambar <adam.bar@brightinventions.pl>
2018-03-20 22:48:39 +0100

    Proper usage of types in generator; simplify generator invocation with ts-node

--- generator/generator.ts
+++ generator/generator.ts
@@ -1,15 +1,39 @@
 import * as fs from 'fs'
-import {camelCase, forEach, pickBy, filter, uniq, map, some} from 'lodash'
+import {camelCase, forEach, pickBy, map, some} from 'lodash'
 
 const fetch = require('node-fetch')
 
-const url = 'https://d3teyb21fexa9r.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json'
+const SchemaUrl = 'https://d3teyb21fexa9r.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json'
 
-function adjustedCamelCase(input) {
+type BasicTypeSuffix = 'Type' | 'ItemType'
+
+interface TypeProperties {
+    Type?: string
+    ItemType?: string
+    PrimitiveType?: string
+    PrimitiveItemType?: string
+    Required: boolean
+}
+
+interface ResourceType {
+    Properties: {[key: string]: TypeProperties}
+}
+
+interface InnerType {
+    Properties: {[key: string]: TypeProperties}
+}
+
+interface Schema {
+    ResourceSpecificationVersion: string
+    ResourceTypes: {[key: string]: ResourceType}
+    PropertyTypes: {[key: string]: InnerType}
+}
+
+function adjustedCamelCase(input: string): string {
     return input === 'IoT' ? 'iot' : camelCase(input)
 }
 
-function determineTypeScriptType(property, propertyName, typeSuffix) {
+function determineTypeScriptType(property: TypeProperties, propertyName: string, typeSuffix: BasicTypeSuffix): string {
     if (property[typeSuffix] === 'List') {
         return `List<${determineTypeScriptType(property, propertyName, 'ItemType')}>`
     }
@@ -23,7 +47,7 @@ function determineTypeScriptType(property, propertyName, typeSuffix) {
         return innerTypeName('.' + property[typeSuffix])
     }
 
-    let primitiveType = property['Primitive' + typeSuffix].toLowerCase()
+    let primitiveType = property[typeSuffix === 'Type' ? 'PrimitiveType' : 'PrimitiveItemType'].toLowerCase()
     if (primitiveType === 'json') {
         return 'any'
     }
@@ -36,8 +60,8 @@ function determineTypeScriptType(property, propertyName, typeSuffix) {
     return `Value<${primitiveType}>`
 }
 
-function propertiesEntries(properties) {
-    return map(properties, (property, propertyName) => {
+function propertiesEntries(properties: {[key: string]: TypeProperties}): string[] {
+    return map(properties, (property: TypeProperties, propertyName: string) => {
         if (propertyName === 'Tags') {
             return `Tags?: ResourceTag[]`
         }
@@ -46,36 +70,9 @@ function propertiesEntries(properties) {
     })
 }
 
-function generateInnerClass(name, properties) {
-    return `export class ${name} {
-${propertiesEntries(properties).map(e => `    ${e}`).join('\n')}
-
-    constructor(properties: ${name}) {
-        Object.assign(this, properties)
-    }
-}`
-}
-
-function generateTopLevelClass(namespace, name, properties, innerTypes) {
-    return `export interface ${name}Properties {
-${propertiesEntries(properties).map(e => `    ${e}`).join('\n')}
-}
-
-export default class ${name} extends ResourceBase {
-${Object.keys(innerTypes).map(innerTypeFullName => {
-    const [, innerTypeNameUnsafe] = innerTypeFullName.split('.')
-    const innerTypeNameSafe = innerTypeName(innerTypeFullName)
-        return `    static ${innerTypeNameUnsafe} = ${innerTypeNameSafe}`
-}).join('\n')}
-
-    constructor(properties?: ${name}Properties) {
-        super('AWS::${namespace}::${name}', properties)
-    }
-}`
-}
-
-function hasTags(properties) {
-    return Object.keys(properties).includes('Tags') || some(properties, p => p.Type === 'List' && p.ItemType === 'Tag')
+function hasTags(properties: {[key: string]: TypeProperties}): boolean {
+    return Object.keys(properties).includes('Tags')
+           || some(properties, p => p.Type === 'List' && p.ItemType === 'Tag')
 }
 
 function innerTypeName(innerTypeFullName: string): string {
@@ -89,9 +86,37 @@ function innerTypeName(innerTypeFullName: string): string {
     return innerTypeName
 }
 
-function generateFile(fileHeader, namespace, resourceName, properties, innerTypes) {
+function generateInnerClass(name: string, properties: {[key: string]: TypeProperties}): string {
+    return `export class ${name} {
+${propertiesEntries(properties).map(e => `    ${e}`).join('\n')}
+
+    constructor(properties: ${name}) {
+        Object.assign(this, properties)
+    }
+}`
+}
+
+function generateTopLevelClass(namespace: string, typeName: string, properties: {[key: string]: TypeProperties}, innerTypes: {[key: string]: InnerType}) {
+    return `export interface ${typeName}Properties {
+${propertiesEntries(properties).map(e => `    ${e}`).join('\n')}
+}
+
+export default class ${typeName} extends ResourceBase {
+${Object.keys(innerTypes).map(innerTypeFullName => {
+        const [, innerTypeNameUnsafe] = innerTypeFullName.split('.')
+        const innerTypeNameSafe = innerTypeName(innerTypeFullName)
+        return `    static ${innerTypeNameUnsafe} = ${innerTypeNameSafe}`
+    }).join('\n')}
+
+    constructor(properties?: ${typeName}Properties) {
+        super('AWS::${namespace}::${typeName}', properties)
+    }
+}`
+}
+
+function generateFile(fileHeader: string, namespace: string, resourceName: string, properties: {[key: string]: TypeProperties}, innerTypes: {[key: string]: InnerType}): void {
     let innerHasTags = false
-    const innerTypesTemplates = map(innerTypes, (innerType, innerTypeFullName) => {
+    const innerTypesTemplates = map(innerTypes, (innerType: InnerType, innerTypeFullName: string) => {
         innerHasTags = innerHasTags || hasTags(innerType.Properties)
         return generateInnerClass(innerTypeName(innerTypeFullName), innerType.Properties)
     })
@@ -120,28 +145,28 @@ ${generatedClass}
     fs.writeFileSync(`./types/${adjustedCamelCase(namespace)}/${camelCase(resourceName)}.ts`, template, {encoding: 'utf8'})
 }
 
-function generateIndexFile(fileHeader, namespace, resourceNames) {
-    const imports = resourceNames.map(resourceName => `import ${resourceName} from './${camelCase(resourceName)}'`)
+function generateIndexFile(fileHeader: string, namespace: string, resourceTypeNames: string[]): void {
+    const imports = resourceTypeNames.map(typeName => `import ${typeName} from './${camelCase(typeName)}'`)
 
     const template = `${fileHeader}
    
 ${imports.join('\n')} 
 
 export default {
-${resourceNames.map(t => `  ${t}`).join(',\n')}
+${resourceTypeNames.map(t => `  ${t}`).join(',\n')}
 }
 `
 
     fs.writeFileSync(`./types/${adjustedCamelCase(namespace)}/index.ts`, template, {encoding: 'utf8'})
 }
 
-function generateGrandIndexFile(fileHeader, indexContent) {
-    const imports = []
+function generateGrandIndexFile(fileHeader: string, indexContent: {[key: string]: string[]}): void {
+    const imports: string[] = []
 
-    forEach(indexContent, (dependentResourceNames, namespace) => {
+    forEach(indexContent, (dependentResourceTypeNames: string[], namespace: string) => {
         imports.push('\n' + `import ${namespace}_ from './${adjustedCamelCase(namespace)}'`)
         imports.push(`export const ${namespace} = ${namespace}_` + '\n')
-        dependentResourceNames.forEach(resourceName => imports.push(`import ${namespace}${resourceName} from './${adjustedCamelCase(namespace)}/${camelCase(resourceName)}'`))
+        dependentResourceTypeNames.forEach(resourceName => imports.push(`import ${namespace}${resourceName} from './${adjustedCamelCase(namespace)}/${camelCase(resourceName)}'`))
     })
 
     const template = `${fileHeader}
@@ -156,25 +181,25 @@ ${Object.keys(indexContent).map(t => `  ${t}`).join(',\n')}
     fs.writeFileSync('./types/index.ts', template, {encoding: 'utf8'})
 }
 
-fetch(url)
-    .then(res => res.json())
-    .then(schema => {
-        const fileHeader = `/* Generated from ${url}, version ${schema.ResourceSpecificationVersion} */`
-        const indexContent = {}
+fetch(SchemaUrl)
+    .then((res: Response) => res.json())
+    .then((schema: Schema) => {
+        const fileHeader = `/* Generated from ${SchemaUrl}, version ${schema.ResourceSpecificationVersion} */`
+        const indexContent: {[key: string]: string[]} = {}
 
-        forEach(schema.ResourceTypes, (resource, resourceFullName) => {
-            const [, namespace, resourceName] = resourceFullName.split('::')
+        forEach(schema.ResourceTypes, (resource: ResourceType, resourceFullName: string) => {
+            const [, namespace, typeName] = resourceFullName.split('::')
             const properties = resource.Properties || {}
-            const resourcePropertyTypes = pickBy(schema.PropertyTypes, (propertyType, propertyFullName) => propertyFullName.startsWith(resourceFullName + '.'))
+            const resourcePropertyTypes = pickBy(schema.PropertyTypes, (propertyType: InnerType, propertyFullName: string) => propertyFullName.startsWith(resourceFullName + '.'))
 
             indexContent[namespace] = indexContent[namespace] || []
-            indexContent[namespace].push(resourceName)
+            indexContent[namespace].push(typeName)
 
-            generateFile(fileHeader, namespace, resourceName, properties, resourcePropertyTypes)
+            generateFile(fileHeader, namespace, typeName, properties, resourcePropertyTypes)
         })
 
-        forEach(indexContent, (resourceNames, namespace) => {
-            generateIndexFile(fileHeader, namespace, resourceNames)
+        forEach(indexContent, (resourceTypeNames: string[], namespace: string) => {
+            generateIndexFile(fileHeader, namespace, resourceTypeNames)
         })
 
         generateGrandIndexFile(fileHeader, indexContent)

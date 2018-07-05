6297a66fa5f8028af92277a1a4caa5c119bda93e
adambar <adam.bar@brightinventions.pl>
2018-07-05 09:31:40 +0200

    Include option to minify the output #9

--- cli/cloudform.ts
+++ cli/cloudform.ts
@@ -2,14 +2,50 @@
 
 import * as path from 'path'
 import {exec} from 'child_process'
+import * as jsonminify from 'jsonminify'
 
-const templatePath = process.argv[2]
+interface CloudformOptions {
+    shouldMinify: boolean
+    templatePath?: string
+}
 
-if (!templatePath || templatePath.startsWith('-')) {
+const options: CloudformOptions = {
+    shouldMinify: false,
+    templatePath: undefined
+}
+
+let shouldShowUsageInfo = true
+
+function parseParam(idx: number) {
+    const param = process.argv[idx]
+
+    if (param.startsWith('-')) {
+        if (param === '-m' || param === '--minify') {
+            options.shouldMinify = true
+        } else {
+            console.warn(`Invalid option ${param}`)
+        }
+    } else {
+        shouldShowUsageInfo = !param
+        options.templatePath = param
+    }
+}
+
+// poor man's CLI arguments reader
+parseParam(2)
+
+if (!options.templatePath) {
+    parseParam(3)
+}
+
+if (shouldShowUsageInfo) {
     console.warn(`cloudform - TypeScript-based imperative way to define AWS CloudFormation templates
 
-usage: cloudform <path>
+usage: cloudform [-m|--minify] <path>
     <path> should point to the TypeScript file containing entrypoint of the CloudFormation definition, including ".ts" suffix.
+
+options:
+    -m, --minify    Minify JSON output, default: false
 
 example: cloudform aws/template.ts > generated.template`)
     process.exit(1)
@@ -17,7 +53,7 @@ example: cloudform aws/template.ts > generated.template`)
 
 // console.info(`Compiling AWS CloudForm template from ${process.argv[2]}...`)
 
-const resolvedTemplatePath = path.resolve(templatePath).replace(/\\/g, "\\\\");
+const resolvedTemplatePath = path.resolve(options.templatePath!).replace(/\\/g, "\\\\");
 
 exec('npm bin', (err, npmBin) => {
     if (err) {
@@ -26,7 +62,7 @@ exec('npm bin', (err, npmBin) => {
     }
 
     const tsNodePath = path.join(npmBin.trim(), 'ts-node')
-    exec(`${tsNodePath} -e "import t from '${path.resolve(resolvedTemplatePath)}'; console.log(t)"`, (err, stdout, stderr) => {
+    exec(`${tsNodePath} -e "import t from '${path.resolve(resolvedTemplatePath)}'; console.log(t)"`, (err, template, stderr) => {
         if (err) {
             console.error(err)
             return
@@ -35,7 +71,12 @@ exec('npm bin', (err, npmBin) => {
             console.error(stderr)
             return
         }
-        console.log(stdout)
+
+        if (options.shouldMinify) {
+            console.log(jsonminify(template))
+        } else {
+            console.log(template)
+        }
     })
 
 })

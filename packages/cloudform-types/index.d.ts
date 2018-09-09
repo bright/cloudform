f9bcf55b21e0de41bd1ec30314fcf461bd12a193
DSP <elfmax@tut.by>
2018-09-10 02:29:23 +0300

    copy index.ts over to cloudform-types

---
+++ packages/cloudform-types/index.d.ts
@@ -0,0 +1,10 @@
+import Template from "./types/template";
+export declare type Template = Template;
+import * as _Fn from './types/functions';
+export declare const Fn: typeof _Fn;
+import * as _Refs from './types/refs';
+export declare const Refs: typeof _Refs;
+export * from './types/index';
+export * from './types/dataTypes';
+export * from './types/resource';
+export * from './types/parameter';
---
+++ packages/cloudform-types/index.ts
@@ -0,0 +1,13 @@
+import Template from "./types/template"
+export type Template = Template
+
+import * as _Fn from './types/functions'
+export const Fn = _Fn
+
+import * as _Refs from './types/refs'
+export const Refs = _Refs
+
+export * from './types/index'
+export * from './types/dataTypes'
+export * from './types/resource'
+export * from './types/parameter'

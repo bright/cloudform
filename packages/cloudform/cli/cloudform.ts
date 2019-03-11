c77a0ace85565f6215d2a15913e7b741166437c8
adambar <adam.bar@brightinventions.pl>
2019-03-11 08:19:01 +0100

    Fix for stdout maxBuffer length exceeded #28

--- packages/cloudform/cli/cloudform.ts
+++ packages/cloudform/cli/cloudform.ts
@@ -62,7 +62,7 @@ exec('npm bin', (err, npmBin) => {
     }
 
     const tsNodePath = path.join(npmBin.trim(), 'ts-node')
-    exec(`${tsNodePath} -e "import t from '${resolvedTemplatePath}'; console.log(t)"`, (err, template, stderr) => {
+    exec(`${tsNodePath} -e "import t from '${resolvedTemplatePath}'; console.log(t)"`, {maxBuffer: 1024 * 1024 * 5}, (err, template, stderr) => {
         if (err) {
             console.error(err)
             return

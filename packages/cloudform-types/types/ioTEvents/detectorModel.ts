a32afc6e2983a9ccced7e398ba6224476886841d
polemius <ivan.menshykov@gmail.com>
2020-05-13 21:04:49 +0200

    Generate types for version 14.2.0

--- packages/cloudform-types/types/ioTEvents/detectorModel.ts
+++ packages/cloudform-types/types/ioTEvents/detectorModel.ts
@@ -1,14 +1,14 @@
 /* Generated from: 
- * ap-northeast-1 (https://d33vqc0rt9ld30.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * ap-northeast-2 (https://d1ane3fvebulky.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * ap-southeast-1 (https://doigdx0kgq9el.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * ap-southeast-2 (https://d2stg8d246z9di.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * eu-central-1 (https://d1mta8qj7i28i2.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * eu-west-1 (https://d3teyb21fexa9r.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * eu-west-2 (https://d1742qcu2c1ncx.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * us-east-1 (https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * us-east-2 (https://dnwj8swjjbsbt.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * us-west-2 (https://d201a2mn26r7lk.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0
+ * ap-northeast-1 (https://d33vqc0rt9ld30.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * ap-northeast-2 (https://d1ane3fvebulky.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * ap-southeast-1 (https://doigdx0kgq9el.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * ap-southeast-2 (https://d2stg8d246z9di.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * eu-central-1 (https://d1mta8qj7i28i2.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * eu-west-1 (https://d3teyb21fexa9r.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * eu-west-2 (https://d1742qcu2c1ncx.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * us-east-1 (https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * us-east-2 (https://dnwj8swjjbsbt.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * us-west-2 (https://d201a2mn26r7lk.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0
  */
    
 import {ResourceBase, ResourceTag} from '../resource'
@@ -34,6 +34,7 @@ export class ResetTimer {
 
 export class IotEvents {
     InputName?: Value<string>
+    Payload?: Payload
 
     constructor(properties: IotEvents) {
         Object.assign(this, properties)
@@ -51,11 +52,69 @@ export class TransitionEvent {
     }
 }
 
+export class AssetPropertyTimestamp {
+    TimeInSeconds?: Value<string>
+    OffsetInNanos?: Value<string>
+
+    constructor(properties: AssetPropertyTimestamp) {
+        Object.assign(this, properties)
+    }
+}
+
+export class DynamoDBv2 {
+    TableName?: Value<string>
+    Payload?: Payload
+
+    constructor(properties: DynamoDBv2) {
+        Object.assign(this, properties)
+    }
+}
+
 export class OnInput {
     Events?: List<Event>
     TransitionEvents?: List<TransitionEvent>
 
     constructor(properties: OnInput) {
+        Object.assign(this, properties)
+    }
+}
+
+export class IotSiteWise {
+    EntryId?: Value<string>
+    PropertyAlias?: Value<string>
+    PropertyValue?: AssetPropertyValue
+    AssetId?: Value<string>
+    PropertyId?: Value<string>
+
+    constructor(properties: IotSiteWise) {
+        Object.assign(this, properties)
+    }
+}
+
+export class AssetPropertyVariant {
+    DoubleValue?: Value<string>
+    IntegerValue?: Value<string>
+    BooleanValue?: Value<string>
+    StringValue?: Value<string>
+
+    constructor(properties: AssetPropertyVariant) {
+        Object.assign(this, properties)
+    }
+}
+
+export class DynamoDB {
+    TableName?: Value<string>
+    PayloadField?: Value<string>
+    RangeKeyField?: Value<string>
+    HashKeyField?: Value<string>
+    RangeKeyValue?: Value<string>
+    RangeKeyType?: Value<string>
+    HashKeyType?: Value<string>
+    HashKeyValue?: Value<string>
+    Payload?: Payload
+    Operation?: Value<string>
+
+    constructor(properties: DynamoDB) {
         Object.assign(this, properties)
     }
 }
@@ -71,9 +130,19 @@ export class SetVariable {
 
 export class Firehose {
     DeliveryStreamName?: Value<string>
+    Payload?: Payload
     Separator?: Value<string>
 
     constructor(properties: Firehose) {
+        Object.assign(this, properties)
+    }
+}
+
+export class Payload {
+    ContentExpression?: Value<string>
+    Type?: Value<string>
+
+    constructor(properties: Payload) {
         Object.assign(this, properties)
     }
 }
@@ -115,8 +184,19 @@ export class Event {
     }
 }
 
+export class AssetPropertyValue {
+    Quality?: Value<string>
+    Value?: AssetPropertyVariant
+    Timestamp?: AssetPropertyTimestamp
+
+    constructor(properties: AssetPropertyValue) {
+        Object.assign(this, properties)
+    }
+}
+
 export class Sns {
     TargetArn?: Value<string>
+    Payload?: Payload
 
     constructor(properties: Sns) {
         Object.assign(this, properties)
@@ -133,6 +213,7 @@ export class OnExit {
 
 export class Sqs {
     UseBase64?: Value<boolean>
+    Payload?: Payload
     QueueUrl?: Value<string>
 
     constructor(properties: Sqs) {
@@ -151,6 +232,7 @@ export class DetectorModelDefinition {
 
 export class IotTopicPublish {
     MqttTopic?: Value<string>
+    Payload?: Payload
 
     constructor(properties: IotTopicPublish) {
         Object.assign(this, properties)
@@ -159,11 +241,14 @@ export class IotTopicPublish {
 
 export class Action {
     IotEvents?: IotEvents
+    Firehose?: Firehose
+    IotTopicPublish?: IotTopicPublish
+    DynamoDB?: DynamoDB
+    DynamoDBv2?: DynamoDBv2
+    IotSiteWise?: IotSiteWise
     ResetTimer?: ResetTimer
     Sqs?: Sqs
-    Firehose?: Firehose
     Sns?: Sns
-    IotTopicPublish?: IotTopicPublish
     SetTimer?: SetTimer
     ClearTimer?: ClearTimer
     Lambda?: Lambda
@@ -176,6 +261,7 @@ export class Action {
 
 export class Lambda {
     FunctionArn?: Value<string>
+    Payload?: Payload
 
     constructor(properties: Lambda) {
         Object.assign(this, properties)
@@ -197,13 +283,20 @@ export default class DetectorModel extends ResourceBase<DetectorModelProperties>
     static ResetTimer = ResetTimer
     static IotEvents = IotEvents
     static TransitionEvent = TransitionEvent
+    static AssetPropertyTimestamp = AssetPropertyTimestamp
+    static DynamoDBv2 = DynamoDBv2
     static OnInput = OnInput
+    static IotSiteWise = IotSiteWise
+    static AssetPropertyVariant = AssetPropertyVariant
+    static DynamoDB = DynamoDB
     static SetVariable = SetVariable
     static Firehose = Firehose
+    static Payload = Payload
     static ClearTimer = ClearTimer
     static OnEnter = OnEnter
     static State = State
     static Event = Event
+    static AssetPropertyValue = AssetPropertyValue
     static Sns = Sns
     static OnExit = OnExit
     static Sqs = Sqs
--- packages/cloudform-types/types/sageMaker/workteam.ts
+++ packages/cloudform-types/types/sageMaker/workteam.ts
@@ -1,10 +1,10 @@
 /* Generated from: 
- * ap-northeast-1 (https://d33vqc0rt9ld30.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * ap-southeast-2 (https://d2stg8d246z9di.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * eu-west-1 (https://d3teyb21fexa9r.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * us-east-1 (https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * us-east-2 (https://dnwj8swjjbsbt.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0,
- * us-west-2 (https://d201a2mn26r7lk.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.0.0
+ * ap-northeast-1 (https://d33vqc0rt9ld30.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * ap-southeast-2 (https://d2stg8d246z9di.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * eu-west-1 (https://d3teyb21fexa9r.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * us-east-1 (https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * us-east-2 (https://dnwj8swjjbsbt.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0,
+ * us-west-2 (https://d201a2mn26r7lk.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json), version 14.2.0
  */
    
 import {ResourceBase, ResourceTag} from '../resource'
